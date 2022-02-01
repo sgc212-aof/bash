91ecd61b0a57f2d518a7c3719a8a5c9744557655
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-02-01 23:03:04 +0300

    Update sample_service_invoke.sh

--- sample_service_invoke.sh
+++ sample_service_invoke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Sample script for background service invocation
 LOG_PATH=log.txt
 CONSOLIDATE_ALL_LOGS=false
 DEV_NULL=/dev/null
-START_SCRIPT=./start.sh
+START_SCRIPT=./sample_background.sh
+
 if [ "$CONSOLIDATE_ALL_LOGS" = "true" ]; then
   PID=`$START_SCRIPT "$@" > $LOG_PATH 2>&1 & echo $!`
   echo logging to file
