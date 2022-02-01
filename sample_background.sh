181281c341c43d45292b5b37659e3d9823af7661
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-02-01 23:02:07 +0300

    Update sample_background.sh

--- sample_background.sh
+++ sample_background.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# This script mimics a sample background script
 echo Started
+# Simply print all the input arguments
 echo $@
 echo Processing
 for i in {1..7}
-  do echo -e ".\c"
+do
+  echo -e '.\c'
   sleep 1
 done
+# Just mimic stderr printing
 echo stderr sample line >&2
 echo Finished
