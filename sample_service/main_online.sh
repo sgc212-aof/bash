ef428b0276167f9c4cf0aef6dfb71a8655bd4b8e
Black Eagle Since 1903 <49439840+black-eagle-1903@users.noreply.github.com>
2022-02-01 23:18:33 +0300

    Update main_online.sh

--- sample_service/main_online.sh
+++ sample_service/main_online.sh
@@ -40,4 +40,5 @@ chmod u+x sample_service_invoke.sh
 # Remove the log file before calling the service
 rm -f log.txt
 ./sample_service_invoke.sh BJK GS FB
+# Watch every second both the log file and ps -ef output
 watch -n 1 -d "tail log.txt;echo '\n';ps -ef"
