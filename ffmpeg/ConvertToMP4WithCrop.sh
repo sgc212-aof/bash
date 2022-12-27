f6a65d8e2df239866e0c31e68e2d275d2279d278
sgc212-aof <120112191+sgc212-aof@users.noreply.github.com>
2022-12-27 11:17:53 +0300

    Create ConvertToMP4WithCrop.sh
    Initial file creation

---
+++ ffmpeg/ConvertToMP4WithCrop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Converts input video files to MP4 format using ffmpeg utility.
+# The following 5 input arguments should be supplied:
+#	1st argument	: Input video file path
+#	2nd argument	: Average video rate
+#	3rd argument	: Maximum video rate
+#	4th argument	: Audio bit rate
+#	5th argument	: Output MP4 video path
+# v1.0
+
+ffmpeg_BIN_PATH=
